41c32d2f81f88906b10ec604c40832baca5a29a6
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-07-31 10:06:39 +0700

    Thêm model Product

---
+++ src/app/models/product.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  id: { type: String, required: true, unique: true },
+  name: { type: String },
+  price: { type: Number },
+  discount: { type: Number },
+  image: [{ type: String }],
+  category: { type: String },
+  brand: { type: String },
+  gender: { type: String },
+  weight: { type: String },
+  stoneMain: { type: String },
+  stoneSecond: { type: String },
+});
+
+module.exports = mongoose.model('Products', productSchema);
