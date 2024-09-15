c689a55ee3c3d2567e27f938171703ac81d3ad3f
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-09-15 16:15:50 +0700

    Update User Controller

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user.model');
 const Token = require('../models/token.model');
@@ -8,7 +9,7 @@ const sendEmail = require('../../utils/sendEmail');
 class UserController {
   async signup(req, res) {
     try {
-      const { email, password } = req.body;
+      const { name, email, password } = req.body;
 
       const existingUser = await User.findOne({ email });
       if (existingUser) {
@@ -20,6 +21,7 @@ class UserController {
       const hashedPass = await bcrypt.hash(password, 10);
 
       const newUser = await new User({
+        name,
         email,
         password: hashedPass,
       }).save();
@@ -37,6 +39,35 @@ class UserController {
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: 'Account sign up failed!' });
+    }
+  }
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+      }
+
+      // Generate a token (you can use JWT for more secure tokens)
+      const token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET, // Make sure to set this environment variable
+        { expiresIn: '1h' } // Token expiration time
+      );
+
+      res.status(200).json({ token, message: 'Login successful!' });
+    } catch (error) {
+      console.error('Login error:', error);
+      res.status(500).json({ message: 'Account log in failed!' });
     }
   }
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const UserController = require('../app/controllers/user.controller');
 
 router.post('/signup', UserController.signup);
+router.post('/login', UserController.login);
 router.post('/update/:id', UserController.updateProfile);
 
 module.exports = router;
